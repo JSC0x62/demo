4dfa59efbe836f138373bb274593544778a1bd4a
JSC0x62 <jscfalcons@gmail.com>
2021-11-17 19:30:55 -0600

    modified class

--- src/main/java/demo/rest/service/impl/MessageServiceImpl.java
+++ src/main/java/demo/rest/service/impl/MessageServiceImpl.java
@@ -3,15 +3,13 @@ package demo.rest.service.impl;
 import demo.rest.domain.Message;
 import demo.rest.service.MessageService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class MessageServiceImpl implements MessageService {
 
     @Override
     public String createMessage(Message message) {
-        return String.format("Hello %s!", message.getText());
+        return message.getText();
     }
 
 }
