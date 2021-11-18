d145efcaaf1fd7086a2293eb0b9b2e235866695a
JSC0x62 <jscfalcons@gmail.com>
2021-11-17 19:16:44 -0600

    added method

---
+++ src/main/java/demo/rest/service/impl/MessageServiceImpl.java
@@ -0,0 +1,14 @@
+package demo.rest.service.impl;
+
+import demo.rest.domain.Message;
+import demo.rest.service.MessageService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageServiceImpl implements MessageService {
+
+    @Override
+    public String createMessage(Message message) {
+        return null;
+    }
+}
