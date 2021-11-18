00615e38a57a7994e367b1d447366af1592087e8
JSC0x62 <jscfalcons@gmail.com>
2021-11-17 19:21:14 -0600

    added method

--- src/main/java/demo/rest/service/impl/MessageServiceImpl.java
+++ src/main/java/demo/rest/service/impl/MessageServiceImpl.java
@@ -3,6 +3,8 @@ package demo.rest.service.impl;
 import demo.rest.domain.Message;
 import demo.rest.service.MessageService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class MessageServiceImpl implements MessageService {
@@ -10,5 +12,11 @@ public class MessageServiceImpl implements MessageService {
     @Override
     public String createMessage(Message message) {
         return null;
+    }
+
+    @PostMapping("/hello")
+    public String hello(@RequestBody Message message) {
+        return String.format("Hello %s!", message.getText());
+
     }
 }
