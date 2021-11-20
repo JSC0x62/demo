fb8577b79de54136b765e152e1e5310a040dc6cd
JSC0x62 <jscfalcons@gmail.com>
2021-11-20 15:07:40 -0600

    added test class

--- src/test/java/demo/rest/HelloWorldControllerTest.java
+++ src/test/java/demo/rest/HelloWorldControllerTest.java
@@ -1,4 +1,45 @@
 package demo.rest;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = Application.class)
+@AutoConfigureMockMvc
+
+
+
 public class HelloWorldControllerTest {
+
+      @Autowired
+      private MockMvc mvc;
+
+
+      @Test
+      public void givenEmployees_whenGetEmployees_thenStatus200()
+              throws Exception {
+
+
+
+            mvc.perform(get("/hello")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content()
+                            .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                    .andExpect((ResultMatcher) jsonPath("$[0].name", is("world")));
+      }
 }
