43ceea6c9bc1336c46d56df6ce2f5cfebe5f9d8d
JSC0x62 <jscfalcons@gmail.com>
2021-11-24 18:42:54 -0600

    modified test classes

--- src/test/java/demo/rest/HelloWorldControllerTest.java
+++ src/test/java/demo/rest/HelloWorldControllerTest.java
@@ -1,16 +1,18 @@
 package demo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
+//import org.springframework.integration.test.util.TestUtils;
+
+
 
 import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,6 +31,8 @@ public class HelloWorldControllerTest {
       @Autowired
       private MockMvc mvc;
 
+      ObjectMapper objectMapper = new ObjectMapper();
+      //objectMapper.writeValue("");
 
       @Test
       public void getTest()
@@ -38,7 +42,7 @@ public class HelloWorldControllerTest {
                             .contentType(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
                     .andExpect(content()
-                            .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                            .contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
                     .andExpect((ResultMatcher) jsonPath("$[0].name", is("world")));
       }
 }
